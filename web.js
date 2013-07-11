0b423b5bf656fd97bdf3de56f23a241b714d7961
Cesar Goudouris <cesar.goudouris@gmail.com>
2013-07-11 18:23:49 +0000

    Improved response code on web.js

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    buff = fs.readFileSync('./index.html');
-    response.send(buff.toString('utf-8'));
+    response.send(fs.readFileSync('./index.html').toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
